f946762ad0b85f7d790f93d5a239b15c26b32916
tedtasman <tktasman@gmail.com>
2025-07-20 18:08:21 -0400

    Structure as package and add documentation

---
+++ docs/build/html/_sources/api.rst.txt
@@ -0,0 +1,42 @@
+Documentation
+=============
+
+Controller
+----------
+
+.. automodule:: MAVez.controller
+   :members:
+   :show-inheritance:
+   :undoc-members:
+
+Coordinate
+----------
+
+.. automodule:: MAVez.coordinate
+   :members:
+   :show-inheritance:
+   :undoc-members:
+
+Flight Controller
+-----------------
+
+.. automodule:: MAVez.flight_controller
+   :members:
+   :show-inheritance:
+   :undoc-members:
+
+Mission
+-------
+
+.. automodule:: MAVez.mission
+   :members:
+   :show-inheritance:
+   :undoc-members:
+
+Mission Item
+------------
+
+.. automodule:: MAVez.mission_item
+   :members:
+   :show-inheritance:
+   :undoc-members:
---
+++ docs/source/conf.py
@@ -0,0 +1,36 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+import os
+import sys
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "MAVez"
+copyright = "2025, The Pennsylvania State University Unmanned Aerial Systems Club"
+author = "The Pennsylvania State University Unmanned Aerial Systems Club"
+release = "1.0.0"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx_autodoc_typehints",
+]
+
+templates_path = ["_templates"]
+exclude_patterns = []
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "furo"
+html_static_path = ["_static"]
+
+# Set the path to the source code directory
+sys.path.insert(0, os.path.abspath("../../src"))
